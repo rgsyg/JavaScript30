e9b42672717c047f115ab18f48b44d795f3cdde2
rigs <rigorsyguatbsn@gmail.com>
2023-08-06 19:34:57 +0800

    refactor: organize code

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,15 +1,26 @@
 window.addEventListener('keydown', (e) => {
+    playAudio(e);
+    manageTransition(e);
+});
+
+function playAudio(e){
     const audio = document.querySelector(`audio[id="${e.code}"]`);
-    const key = document.querySelector(`.key[id="${e.code}"]`);
 
     if (!audio) return;
 
     audio.currentTime = 0;
     audio.play();
+}
+
+function manageTransition(e){
+    const key = document.querySelector(`.key[id="${e.code}"]`);
+
+    if (!key) return;
 
     key.classList.add('playing');
+
     key.addEventListener('transitionend', e => {
         if (e.propertyName !== 'transform') return;
         key.classList.remove('playing');
     });
-});
+}
